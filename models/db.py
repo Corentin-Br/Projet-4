aee8a29a34571579acf8383f7b881ac69431469b
Corentin BRAVO <corentinbravo@gmail.com>
2021-05-20 14:42:20 +0200

    Changed the name of the former classes module to core.

--- models/db.py
+++
@@ -1,6 +0,0 @@
-from tinydb import TinyDB, Query
-
-database = TinyDB("models/db.json")
-tournament_tables = database.table("tournaments")
-member_tables = database.table("members")
-query = Query()
